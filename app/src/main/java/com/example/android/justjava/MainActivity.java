cd6a8d2a090b753e3d70df152cbdeacd6c4f4d39
Marchis Alex :D <a.marchis@yahoo.com>
2016-04-13 17:23:48 +0300

    Coffe order app draft 3

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.android.justjava;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    int i=0;
+    public void submitOrder(View view) {
+
+
+        displayPrice(5 * i);
+
+    }
+    public void increment(View View){
+        i=i+1;
+        display(i);
+
+
+
+    }
+    public void descrement(View View){
+        i=i-1;
+        display(i);
+
+
+    }
+
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+
+    }
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+}
